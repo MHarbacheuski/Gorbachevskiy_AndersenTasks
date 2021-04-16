81560d7db7b99ef80c76e8767f82faf9e928281a
Maxim Gorbachevskiy <46083782+Skuuukaaa@users.noreply.github.com>
2021-04-16 16:45:57 +0300

    red

---
+++ Gorbachevskiy_AndersenTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AndersenTasks
+{
+    class Program
+    {
+        /// <summary>
+        /// Составить алгоритм: если введенное число больше 7, то вывести “Привет”
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите число: ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    if (n > 7)
+                    {
+                        Console.WriteLine("Привет");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите число больше 7");
+                    }
+                }
+                catch (Exception p)
+                {
+                    Console.WriteLine("Некорретный ввод! Введите число");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
